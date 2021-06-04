6010578edc4b4aff1d2bfca0d9aef4bc5c8f6456
Akib <akib.reza23@gmail.com>
2021-06-04 22:15:02 +0600

    Finished creating quote array

--- js/script.js
+++ js/script.js
@@ -9,14 +9,14 @@ const quotes = [
   {
     quote: "Be the change that you wish to see in the world.",
     source: "Mahatma Gandhi",
-    citation: "Speech",
+    citation: "Philosophy",
     year: 1947,
   },
   {
     quote: "There is only one good, knowledge, and one evil, ignorance.",
     source: "Socrates",
     citation: "Philosophy",
-    year: 1947,
+    year: "399 BC",
   },
   {
     quote: "I dream my painting and I paint my dream.",
@@ -25,10 +25,48 @@ const quotes = [
     year: 1890,
   },
   {
-    quote: "There is only one good, knowledge, and one evil, ignorance.",
-    source: "Socrates",
+    quote: "If you tell the truth, you don't have to remember anything.",
+    source: "Mark Twain",
+    citation: "Literature",
+    year: 1910,
+  },
+  {
+    quote: "We are all in the gutter, but some of us are looking at the stars.",
+    source: "Oscar Wilde",
+    citation: "Lady Windermere's Fan",
+    year: 1900,
+  },
+  {
+    quote: "Peace begins with a smile.",
+    source: "Mother Teresa",
+    citation: "Spirituality",
+    year: 1997,
+  },
+  {
+    quote:
+      "Everyone thinks of changing the world, but no one thinks of changing himself.",
+    source: "Leo Tolstoy",
     citation: "Philosophy",
-    year: "399 BCE",
+    year: 1910,
+  },
+  {
+    quote:
+      "You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one.",
+    source: "John Lennon",
+    citation: "Music",
+    year: 1980,
+  },
+  {
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    source: "Confucius",
+    citation: "Philosophy",
+    year: "479 BC",
+  },
+  {
+    quote: "It always seems impossible until it's done.",
+    source: "Nelson Mandela",
+    citation: "Social Science",
+    year: 2013,
   },
 ];
 
