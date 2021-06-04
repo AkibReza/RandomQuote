38d8cbe0a8554ab6a570c062db50818aa80762b3
Akib <akib.reza23@gmail.com>
2021-06-04 21:06:43 +0600

    Creating quotes array

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/******************************************
+project 1 - Random Quote Generator
+******************************************/
+
+/***
+ * `quotes` array
+ ***/
+const quotes = [
+  {
+    quote: "Be the change that you wish to see in the world.",
+    source: "Mahatma Gandhi",
+    citation: "Speech",
+    year: 1947,
+  },
+  {
+    quote: "There is only one good, knowledge, and one evil, ignorance.",
+    source: "Socrates",
+    citation: "Philosophy",
+    year: 1947,
+  },
+  {
+    quote: "I dream my painting and I paint my dream.",
+    source: "Vincent Van Gogh",
+    citation: "Art",
+    year: 1890,
+  },
+  {
+    quote: "There is only one good, knowledge, and one evil, ignorance.",
+    source: "Socrates",
+    citation: "Philosophy",
+    year: "399 BCE",
+  },
+];
+
+/***
+ * `getRandomQuote` function
+ ***/
+
+/***
+ * `printQuote` function
+ ***/
+
+/***
+ * click event listener for the print quote button
+ ***/
